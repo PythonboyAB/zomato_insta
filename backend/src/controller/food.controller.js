3720c5319af9e352cd26b9383579e4003f96498b
Abhishek Sahani <abh792002@gmail.com>
2025-10-23 23:15:07 +0530

    today's update

--- backend/src/controller/food.controller.js
+++ backend/src/controller/food.controller.js
@@ -1,9 +1,32 @@
 import foodModel from "../models/food.model.js";
+import uploadFile from "../services/storage.service.js";
+import { v4 as uuid } from "uuid";
 
 async function createFood(req, res) {
-    console.log(req.foodPartner);
-    res.send("created the food item");
-}
+  try {
+    if (!req.file) {
+      return res.status(400).json({ message: "No file uploaded" });
+    }
 
+    const fileUploadResult = await uploadFile(req.file.buffer, uuid());
+
+    const foodItem = await foodModel.create({
+      name: req.body.name,
+      description: req.body.description,
+      video: fileUploadResult.url,
+      foodPartner: req.foodPartner._id,
+      
+    });
+    console.log("BODY:", req.body);
+    console.log("FILE:", req.file);   
+    res.status(201).json({
+      message: "Food created successfully",
+      food: foodItem,
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Error creating food", error: error.message });
+  }
+}
 
 export default createFood;
--- backend/src/routes/food.route.js
+++ backend/src/routes/food.route.js
@@ -1,10 +1,12 @@
 import { Router } from "express";
+import multer from "multer";
 import authFoodPartnerMiddleware from "../../middleware/auth.middleware.js";
 import createFood from "../controller/food.controller.js";
 
 const router = Router();
+const upload = multer({ storage: multer.memoryStorage() });
 
-router.post("/", authFoodPartnerMiddleware, createFood);
-
+// 👇 This must be in this exact order
+router.post("/", authFoodPartnerMiddleware, upload.single("video"), createFood);
 
 export default router;
